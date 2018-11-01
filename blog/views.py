2e6df2fd50193728456826cffc7dfd0bb5843dd4
Alex <harrison.alex117@gmail.com>
2018-11-01 13:20:20 -0700

    simple template html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+#creating the view, post_list, from /blog/urls.py
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
